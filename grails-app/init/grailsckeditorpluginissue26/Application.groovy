680be554968cef8c1683b1dfa6e7a057d0a7cac9
Moritz Kobel <moritz.kobel@itds.ch>
2016-05-24 18:49:31 +0200

    try to break ckeditor

--- grails-app/init/grailsckeditorpluginissue26/Application.groovy
+++ grails-app/init/grailsckeditorpluginissue26/Application.groovy
@@ -3,6 +3,12 @@ package grailsckeditorpluginissue26
 import grails.boot.GrailsApp
 import grails.boot.config.GrailsAutoConfiguration
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.boot.autoconfigure.security.SecurityFilterAutoConfiguration
+
+@ComponentScan
+@EnableAutoConfiguration(exclude = [SecurityFilterAutoConfiguration])
 class Application extends GrailsAutoConfiguration {
     static void main(String[] args) {
         GrailsApp.run(Application, args)
